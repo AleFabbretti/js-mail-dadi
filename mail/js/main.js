7395b309656afe2de5654d9f9570f7fdcfb0b06e
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-25 15:56:54 +0200

    Aggiunto condizione di verifica mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -6,7 +6,7 @@ stampa un messaggio appropriato sull’esito del controllo.
 */
 
 // crea una lista di mail
-const validMails = [ "alessandro.fabbretti@gmail.com", "marcorossi@libero.it", "luca.bianchi@esempio.com"];
+const validMails = ["alessandro.fabbretti@gmail.com", "marcorossi@libero.it", "luca.bianchi@esempio.com"];
 console.log(validMails);
 
 //chiedi all'utente la sua mail
@@ -14,3 +14,19 @@ const userMail = prompt("Inserisci la tua mail");
 console.log(userMail);
 
 //controlla che sia nella lista di chi può accedere
+for ( let i = 0; i < validMails.length; i++) {
+    const mail = validMails[i];
+
+    //aggiungo condizione di verifica
+    if ( userMail === mail){
+        alert("La tua E-mail è valida")
+
+        break;
+    } else {
+        alert("La tua E-mail non è corretta! Per favore riprova")
+
+        break;
+    }
+}
+
+
