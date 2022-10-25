2ff5e7cae58ce3dccbe91c8b69d6560bc7bf7496
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-25 15:33:05 +0200

    Aggiunta lista di mail valide

---
+++ mail/js/main.js
@@ -0,0 +1,12 @@
+/*
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+// crea una lista di mail
+const validMails = [ "alessandro.fabbretti@gmail.com", "marcorossi@libero.it", "luca.bianchi@esempio.com"];
+console.log(validMails);
+
+
