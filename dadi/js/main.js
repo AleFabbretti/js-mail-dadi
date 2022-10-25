226710816dc0869f94c93301a950785b6b1ce609
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-25 16:49:13 +0200

    Aggiunta condizione per assegnare vittoria

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -6,4 +6,19 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 //dado giocatore utente - aggiungo variabile
 let userNut = Math.floor(Math.random() * 6) + 1;
+console.log(userNut);
 
+//dado cpu - aggiungo variabile
+
+let cpuNut = Math.floor(Math.random() * 6) + 1;
+console.log(cpuNut);
+
+//aggiungo condizione per aggiudicare la vittoria
+
+if ( userNut > cpuNut ) {
+    alert (`HAI VINTO!!!`)
+} else if ( userNut === cpuNut) {
+    alert (`Hai pareggiato! Ritenta!`)
+} else {
+    alert (`Mi dispiace, Hai perso!Ritenta!`)
+}
