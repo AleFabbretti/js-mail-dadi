36f08770199a88341b17a148dc17fd040137c191
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-25 16:17:05 +0200

    Aggiungo variabile dado utente

---
+++ dadi/js/main.js
@@ -0,0 +1,9 @@
+/*
+Generare un numero random da 1 a 6, 
+sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+//dado giocatore utente - aggiungo variabile
+let userNut = Math.floor(Math.random() * 6) + 1;
+
