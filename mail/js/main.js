64edee7c329ab8b40851ee8dc27ac9d28d53c2c8
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-25 15:41:14 +0200

    Aggiunta richiesta mail all'utente

--- mail/js/main.js
+++ mail/js/main.js
@@ -9,4 +9,8 @@ stampa un messaggio appropriato sull’esito del controllo.
 const validMails = [ "alessandro.fabbretti@gmail.com", "marcorossi@libero.it", "luca.bianchi@esempio.com"];
 console.log(validMails);
 
+//chiedi all'utente la sua mail
+const userMail = prompt("Inserisci la tua mail");
+console.log(userMail);
 
+//controlla che sia nella lista di chi può accedere
